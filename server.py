ed6583b82274ca18ec138c8ef168011900e75040
Flajt <matrimkurz@gmail.com>
2017-02-05 15:29:04 +0100

    Add more
    Add the addpic() that allows to add a picture to the server and improve
    the code and i create a Basic server script but i doesnt do anything

--- server.py
+++ server.py
@@ -1,5 +1,10 @@
 import socket
 import os
+mail_path="/Desktop/mail"
+picture_path="/Desktop/picture"
+Video_path="/Desktop/video"
+
+
 
 # a server script dont know what it does ; )
 server_ip="192.168.12"
@@ -9,4 +14,7 @@ serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind((socket.gethostname(), 80))
 serversocket.listen(5)
 
-mail=socket.fromshare(info=1)
+test=socket.fromshare(1240)
+
+if test=="mail.pkl":
+    os.mkdir(mail_path)
--- ui.py
+++ ui.py
@@ -2,12 +2,15 @@ ok=False
 import sys
 import pickle
 import os
+import time
+import getpass
 from passlib.hash import pbkdf2_sha256
 global x
+usern=getpass.getuser()
 x=0
 password1=False
 password2=None
-path="/Desktop"
+path="C:/Users/"+usern+"/Desktop/pictures/"
 curdir=os.getcwd()
 print(curdir)
 
@@ -41,11 +44,47 @@ def Help():                     #help part
         main()
 
 
+def addpic():           #the add Picture function
+    print(os.getcwd())
+    print("Enter the name of the picture")
+    try:
+        t=os.chdir(path)    #test if the dir exist
+    except OSError:
+        print("error")
+        t=False
+    if t==False:
+        os.chdir("C:/Users/"+usern+"/Desktop")
+        os.mkdir("pictures")
+        print(os.getcwd())
+    print(os.getcwd())
+    picname=input("Enter the picture name: ")
+    try:
+        os.chdir(path)
+        t=open(picname,"r")
+    except( FileExistsError, FileNotFoundError):
+        print("The file could not found try it aggain")
+        q=input("Do you want to continue? [y/n]")
+        if q=="y":
+            addpic()
+        if q=="n":
+            print("")
+            main()
+
+    print("Picture found, Data will send pls wait...")
+    m=input("Do you want to continue? [y/n]")
+    if m=="y":
+        addpic()
+    if m=="n":
+        print("")
+        print("")
+        main()
 
 
 
 
 def main():         #main part with options
+    global user
+    global passw
     print("1:Add picture")
     print("2:Delete picture")
     print("3:Add mail Adress")
@@ -61,11 +100,11 @@ def main():         #main part with options
     if wish==4:
         Help()
     if wish==5:
-        passw.close()
-        user.close()
         time.sleep(1)
         sys.exit()
         exit()
+
+
 
 
 def create():
@@ -86,58 +125,47 @@ def create():
         hash = pbkdf2_sha256.encrypt(password2, rounds=200000, salt_size=16)
         password=open("pass.pkl","wb")
         save2=pickle.dump(hash, password)
-        main()
 
 
     mail=input("Enter your Mail adress:")   #more important infos
     password=open("pass.pkl","wb")
     mail2=open("mail.pkl","wb")
-    save2=pickle.dump(password,var)
     save3=pickle.dump(mail,mail2)
-    fileObject.close()
+    username.close()
     password.close()
+    print("")
+    main()
 
-def login():
+
+
+
+def login():    #is the login menue
     global x
+    global hash
     while x!=3:
         Pass=input("Enter your Password: ")
         right=pbkdf2_sha256.verify(Pass, hash)
         if right==True:
             print("Welcome back "+user)
+            print("")
             main()
-        else:
-            x=x+1
+
+    if right==False:
+        x=x+1
+
         if x==3:
             print("That goes Wrong...")
             exit()
             sys.exit()
 
 try:
-  hash=pickle.load(open("pass.pkl","rb"))               #this is the part were everything Load dont know if that is working
-  user=pickle.load(open("username.pkl","rb"))
-  mail=pickle.load(open("mail.pkl","rb"))
-  ok=True
-  print(hash)
-except (FileExistsError, FileNotFoundError): 
+    hash=pickle.load(open("pass.pkl","rb"))               #this is the part were everything Load dont know if that is working
+    user=pickle.load(open("username.pkl","rb"))
+    mail=pickle.load(open("mail.pkl","rb"))
+    ok=True
+except(FileExistsError, FileNotFoundError):
     print("Loading Error")
     create()
-
-def login():
-    passw=open("pass.pkl",)
-    user=open("fileObejct.pkl")
-    print(user)
-    while x!=3:
-        passw2=input("Enter your Password: ")
-        if pasw==passw2:
-            main()
-        else:
-            x=x+1
-    if x==3:
-        print("That goes wrong try it later")
-        import sys
-        sys.exit()
-        exit()
-
 
 
 
@@ -148,25 +176,3 @@ else:
     print("Load error") # this is the error if the file cant Load
     exit()
     sys.exit()
-
-
-
-
-
-
-
-def addpic():
-    print("Enter the name of the picture")
-    print("If you use that the first time pls add a folder on your Desktop and put every Picture in that ")
-    picname=input("Enter the picture name")
-    try:
-        t=open(pciname,"r")
-    except( FileExistsError, FileNotFoundError):
-        print("The file could not found try it aggain")
-    if t==True:
-        print("Picture found Data will send pls wait...")
-
-    else:
-        while t!=open:
-            picname=input("Enter the pictures name: ")
-            t=open(picname, "r")
