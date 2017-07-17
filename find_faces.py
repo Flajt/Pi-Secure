5e92bf6bacd88ffe0388ab1ec15e1de9bf5f9e18
Flajt <matrimkurz@gmail.com>
2017-07-17 16:14:16 +0200

    Add find Faces
    This file create a info.txt with the excat position of the faces in
    their.

---
+++ find_faces.py
@@ -0,0 +1,30 @@
+import cv2 as cv
+import os
+import argparse
+import numpy as np
+import time
+
+dire="pos"
+line=0
+faceCascade = cv.CascadeClassifier("D:\DOWNLOADS\opencv\sources\data\haarcascades_GPU\haarcascade_frontalface_default.xml")
+
+for image in os.listdir(dire):
+    read=cv.imread("pos/"+image)
+    convert_gray=cv.cvtColor(read, cv.COLOR_BGR2GRAY)
+    new=cv.resize(convert_gray, (300, 300))
+    cv.imwrite("pos/"+image, new)
+    faces = faceCascade.detectMultiScale(convert_gray,scaleFactor=1.3,minNeighbors=5,minSize=(30, 30))
+    for (x,y,w,h) in faces:
+            cv.rectangle(read, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cur_dir=os.getcwd()
+            text=str(cur_dir)+"/"+"pos/"+image+" 1"+" "+str(x)+" "+str(y)+" "+str(w)+" "+str(h)+"\n"
+            File=open("info.txt","a")
+            File.write(text)
+
+            cv.waitKey(0)
+            #time.sleep(0.5)
+            cv.imshow("Note: This Programm is not 100%, correct!" ,read)
+
+
+
+File.close()
