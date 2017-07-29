40274ebd0fb9ccdd4743fb0b100ca1613f0231be
Flajt <matrimkurz@gmail.com>
2017-07-29 17:04:50 +0200

    Improve the code
    First render and then find the faces

--- find_faces.py
+++ find_faces.py
@@ -4,6 +4,7 @@ import argparse
 import numpy as np
 import time
 
+
 dire="pos"
 line=0
 faceCascade = cv.CascadeClassifier("D:\DOWNLOADS\opencv\sources\data\haarcascades_GPU\haarcascade_frontalface_default.xml")
@@ -13,7 +14,10 @@ for image in os.listdir(dire):
     convert_gray=cv.cvtColor(read, cv.COLOR_BGR2GRAY)
     new=cv.resize(convert_gray, (300, 300))
     cv.imwrite("pos/"+image, new)
-    faces = faceCascade.detectMultiScale(convert_gray,scaleFactor=1.3,minNeighbors=5,minSize=(30, 30))
+
+for image in os.listdir(dire):
+    output=cv.imread("pos/"+image)
+    faces = faceCascade.detectMultiScale(output,scaleFactor=1.3,minNeighbors=5,minSize=(30, 30))
     for (x,y,w,h) in faces:
             cv.rectangle(read, (x, y), (x+w, y+h), (0, 255, 0), 2)
             cur_dir=os.getcwd()
@@ -21,10 +25,8 @@ for image in os.listdir(dire):
             File=open("info.txt","a")
             File.write(text)
 
-            cv.waitKey(0)
-            #time.sleep(0.5)
-            cv.imshow("Note: This Programm is not 100%, correct!" ,read)
-
-
+            cv.waitKey(1)
+            time.sleep(0.5)
+            cv.imshow("Note: This Programm is not 100%, correct!" ,output)
 
 File.close()
